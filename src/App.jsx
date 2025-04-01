66cd613148536a7f9d89818116a235d871a406a7
KnightHub616 <chabarifamily@gmail.com>
2025-04-01 12:22:56 -0600

    deployed app for block24 in readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { puppyList } from "./data";
+import "./index.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  return <>
-  </>
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
-export default App
+
+export default App;
